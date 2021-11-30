90ee39f72961097d477e0b953b53b2d565f0c9ed
royquillca <ccus.andy.quillca@gmail.com>
2021-11-30 01:55:50 -0500

    Mi primera página creada

---
+++ app.js
@@ -0,0 +1,44 @@
+// const http = require('http')
+// http.createServer(function (req, res){
+//     res.end("Hello World")
+// }).listen(3000)
+
+const express = require('express')
+const app = express()
+
+
+// app.use((req, res, next) => {
+    // console.log('Nuevo visitante')
+//     console.log(req.url)
+//     next()
+// })
+
+// app.use(function (req, res) {
+    // res.send('Hello World')
+    // console.log(__dirname)
+    // res.sendFile(__dirname + '/mipagina.html')
+// })
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/mipagina.html')
+})
+
+app.get('/muestra-imagen', (req, res) => {
+    res.sendFile(__dirname + '/js-logo.png')
+})
+
+app.get('/about', (req, res) =>{
+    res.send("About")
+})
+
+app.get('/habilidades', (req, res) => {
+    res.send("Habilidades")
+})
+
+app.listen(process.env.PORT || 3000)
+
+
+
+
+
+
